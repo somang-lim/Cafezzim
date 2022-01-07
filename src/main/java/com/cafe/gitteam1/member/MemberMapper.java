6aa17f74a0e7577ef894ba29828aa5f5bd178bd6
imhope <limsomang628@naver.com>
2022-01-07 10:30:14 +0900

    로그인 기본 back-end 구현 -> xml 이름 수정 & 주석 추가

--- src/main/java/com/cafe/gitteam1/member/MemberMapper.java
+++ src/main/java/com/cafe/gitteam1/member/MemberMapper.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface MemberMapper {
+	// MemberService에서 사용
 	public boolean signInCheck(MemberVo vo, HttpSession session);
+	public void signOut(HttpSession session);
 	
-	public String signInCheck(MemberVo vo);
-	
+	// xml(SQL)에서 사용
+	public String signInCheck(MemberVo vo);	
 	public MemberVo viewMember(MemberVo vo);
 	
-	public void signOut(HttpSession session);
 }
